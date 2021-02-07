663f1e3526917c972cb4cb391a2426183ad8f508
zanderson1998 <zanderson1998@yahoo.com>
2021-02-07 16:33:35 -0700

    prompt user input

--- db/database.js
+++ db/database.js
@@ -11,7 +11,19 @@ const db = mysql.createConnection({
 db.connect(err => {
     if (err) throw err;
     console.log('connected as id ' + db.threadId + '\n');
-    db.end();
+    promptUser();
 });
+
+promptUser = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'openingList',
+            message: 'Welcome to Employee Tracker \n What would you like to do?',
+            choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add A Department', 'Add A Role', 'Add An Employee', 'Update An Employee Role']
+        }
+    ]);
+    db.end();
+};
 
 module.exports = db;
--- db/schema.sql
+++ db/schema.sql
@@ -4,28 +4,31 @@ CREATE DATABASE employee_tracker;
 
 USE employee_tracker;
 
+CREATE TABLE department (
+    departmentId INT AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (departmentId)
+);
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(departmentId)
+);
+
 CREATE TABLE employees (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER UNSIGNED NOT NULL,
-    manager_id INTEGER UNSIGNED,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
     PRIMARY KEY (id),
-    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (role_id) REFERENCES roles(id),
     FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
 
-CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INTEGER UNSIGNED,
-    PRIMARY KEY (id),
-    FOREIGN KEY (department_id) REFERENCES department(id)
-);
 
-CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
-)
+
