9d9777f9c81a7f7c4f9d32b65b2416e69bef7644
zanderson1998 <zanderson1998@yahoo.com>
2021-02-07 21:05:16 -0700

    modularize the code

--- db/database.js
+++ db/database.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
+const {viewAllDepartments,viewAllRoles,viewAllEmployees,addADepartment,addARole,addAnEmployee,updateAnEmployeeRole,quit} = require('../utils/primaryFunctions');
 
 //create connection to database
 const db = mysql.createConnection({
@@ -33,214 +34,93 @@ userResponseHandler = (res) => {
     console.log(res.openingList);
     const userResponse = res.openingList;
 
-    //Api calls based upon user response
-    if (userResponse === "View All Departments") {
-        viewAllDepartments();
-    }
-    if (userResponse === "View All Roles") {
-        viewAllRoles();
-    }
-    if (userResponse === "View All Employees") {
-        viewAllEmployees();
-    }
-    if (userResponse === "Add A Department") {
-        inquirer.prompt([
-            {
-                type: 'input',
-                name: 'name',
-                message: 'Please enter the department name.'
-            }
-        ]).then(ans => addADepartment(ans));
-    }
-    if (userResponse === "Add A Role") {
-        inquirer.prompt([
-            {
-                type: 'input',
-                name: 'title',
-                message: 'Please enter the role title.'
-            },
-            {
-                type: 'input',
-                name: 'salary',
-                message: "Please enter the role's salary."
-            },
-            {
-                type: 'input',
-                name: 'department_id',
-                message: "Please enter the role's department id."
-            },
-        ]).then(ans => addARole(ans));
-    }
-    if (userResponse === "Add An Employee") {
-        inquirer.prompt([
-            {
-                type: 'input',
-                name: 'first_name',
-                message: "Please enter the employee's first name."
-            },
-            {
-                type: 'input',
-                name: 'last_name',
-                message: "Please enter the employee's last name."
-            },
-            {
-                type: 'input',
-                name: 'role_id',
-                message: "Please enter the employee's role id."
-            },
-            {
-                type: 'input',
-                name: 'manager_id',
-                message: "Please enter the employee's manager id."
-            }
-        ]).then(ans => addAnEmployee(ans));
-    }
-    if (userResponse === "Update An Employee Role") {
-        inquirer.prompt([
-            {
-                type: 'input',
-                name: 'employee_id',
-                message: "Please enter the employee's id."
-            },
-            {
-                type: 'input',
-                name: 'employee_role',
-                message: "Please enter the employee's updated role."
-            }
-        ]).then(ans => updateAnEmployeeRole(ans));
-    }
-    if (userResponse === "Quit") {
-        db.end()
-    }
-};
+    switch (userResponse) {
+        case 'View All Departments':
+            viewAllDepartments();
+            break;
 
-//view all departments
-viewAllDepartments = () => {
-    console.log("Viewing all departments");
-    db.query(
-        'SELECT * FROM department',
-        function (err, res) {
-            if (err) throw err;
-            console.table(res);
-        }
-    )
-    db.end();
-};
+        case 'View All Roles':
+            viewAllRoles();
+            break;
 
-//view all roles
-viewAllRoles = () => {
-    console.log("Viewing all roles");
-    db.query(
-        'SELECT * FROM roles',
-        function (err, res) {
-            if (err) throw err;
-            console.table(res);
-        }
-    )
-    db.end();
-};
+        case 'View All Employees':
+            viewAllEmployees();
+            break;
 
-//view all employees
-viewAllEmployees = () => {
-    console.log("Viewing all employees");
-    db.query(
-        'SELECT * FROM employees',
-        function (err, res) {
-            if (err) throw err;
-            console.table(res);
-        }
-    )
-    db.end();
-};
+        case 'Add A Department':
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'name',
+                    message: 'Please enter the department name.'
+                }
+            ]).then(ans => addADepartment(ans))
+            break;
 
-//add a department
-addADepartment = (ans) => {
-    db.query(
-        'INSERT INTO department SET ?',
-        ans,
-        function (err, res) {
-            if (err) throw err;
-            console.log(res.affectedRows + ' department added!\n');
-        }
-    )
-    db.end();
-};
+        case 'Add A Role':
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'title',
+                    message: 'Please enter the role title.'
+                },
+                {
+                    type: 'input',
+                    name: 'salary',
+                    message: "Please enter the role's salary."
+                },
+                {
+                    type: 'input',
+                    name: 'department_id',
+                    message: "Please enter the role's department id."
+                },
+            ]).then(ans => addARole(ans));
+            break;
 
-//add a role
-addARole = (ans) => { 
-    let salary = parseInt(ans.salary);
-    let departmentId = parseInt(ans.department_id);
-    db.query(
-        'INSERT INTO roles SET ?',
-        {
-            title: ans.title,
-            salary: salary,
-            department_id: departmentId
-        },
-        function (err, res) {
-            if (err) throw err;
-            console.log(res.affectedRows + ' role added!\n');
-        }
-    )
-    db.end();
-};
+        case 'Add An Employee':
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'first_name',
+                    message: "Please enter the employee's first name."
+                },
+                {
+                    type: 'input',
+                    name: 'last_name',
+                    message: "Please enter the employee's last name."
+                },
+                {
+                    type: 'input',
+                    name: 'role_id',
+                    message: "Please enter the employee's role id."
+                },
+                {
+                    type: 'input',
+                    name: 'manager_id',
+                    message: "Please enter the employee's manager id."
+                }
+            ]).then(ans => addAnEmployee(ans));
+            break;
+        case 'Update An Employee Role':
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'employee_id',
+                    message: "Please enter the employee's id."
+                },
+                {
+                    type: 'input',
+                    name: 'employee_role',
+                    message: "Please enter the employee's updated role."
+                }
+            ]).then(ans => updateAnEmployeeRole(ans));
+            break;
 
-//add an employee
-addAnEmployee = (ans) => { 
-    let roleId = ans.role_id;
-    let managerId = ans.manager_id;
-    db.query(
-        'INSERT INTO employees SET ?',
-        {
-            first_name: ans.first_name,
-            last_name: ans.last_name,
-            role_id: roleId,
-            manager_id: managerId
-        },
-        function (err, res) {
-            if (err) throw err;
-            console.log(res.affectedRows + ' employee added!\n');
-        }
-    )
-    db.end();
-};
-
-//update an employee
-updateAnEmployeeRole = (ans) => { 
-    let employeeId = parseInt(ans.employee_id);
-    let newRole = parseInt(ans.employee_role);
-    db.query(
-        'UPDATE employees SET ? WHERE ?',
-        [
-            {role_id: newRole},
-            {id: employeeId}
-        ],
-        function (err, res) {
-            if (err) throw err;
-            console.log(res.affectedRows + " employee's role updated!\n");
-        }
-    )
-    db.end();
-};
-
-quit = () => {
-    inquirer.prompt([
-        {
-            type: 'confirm',
-            name: 'return',
-            message: 'Go Back To Main Menu?',
-            default: false
-        }
-    ])
-        .then(ans => {
-            if (ans.return) {
-                promptUser();
-            } else {
-                console.log("Goodbye");
-                db.end();
-            }
-        });
-};
+        case 'Quit':
+            db.end();
+            break;
+    }
+}
 
 
 module.exports = db;
---
+++ utils/primaryFunctions.js
@@ -0,0 +1,142 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'zack98**',
+    database: 'employee_tracker'
+});
+
+//view all departments
+viewAllDepartments = () => {
+    console.log("Viewing all departments");
+    db.query(
+        'SELECT * FROM department',
+        function (err, res) {
+            if (err) throw err;
+            console.table(res);
+        }
+    )
+};
+
+//view all roles
+viewAllRoles = () => {
+    console.log("Viewing all roles");
+    db.query(
+        'SELECT * FROM roles',
+        function (err, res) {
+            if (err) throw err;
+            console.table(res);
+        }
+    )
+};
+
+//view all employees
+viewAllEmployees = () => {
+    console.log("Viewing all employees");
+    db.query(
+        'SELECT * FROM employees',
+        function (err, res) {
+            if (err) throw err;
+            console.table(res);
+        }
+    )
+};
+
+//add a department
+addADepartment = (ans) => {
+    db.query(
+        'INSERT INTO department SET ?',
+        ans,
+        function (err, res) {
+            if (err) throw err;
+            console.log(res.affectedRows + ' department added!\n');
+        }
+    )
+};
+
+//add a role
+addARole = (ans) => { 
+    let salary = parseInt(ans.salary);
+    let departmentId = parseInt(ans.department_id);
+    db.query(
+        'INSERT INTO roles SET ?',
+        {
+            title: ans.title,
+            salary: salary,
+            department_id: departmentId
+        },
+        function (err, res) {
+            if (err) throw err;
+            console.log(res.affectedRows + ' role added!\n');
+        }
+    )
+};
+
+//add an employee
+addAnEmployee = (ans) => { 
+    let roleId = ans.role_id;
+    let managerId = ans.manager_id;
+    db.query(
+        'INSERT INTO employees SET ?',
+        {
+            first_name: ans.first_name,
+            last_name: ans.last_name,
+            role_id: roleId,
+            manager_id: managerId
+        },
+        function (err, res) {
+            if (err) throw err;
+            console.log(res.affectedRows + ' employee added!\n');
+        }
+    )
+};
+
+//update an employee
+updateAnEmployeeRole = (ans) => { 
+    let employeeId = parseInt(ans.employee_id);
+    let newRole = parseInt(ans.employee_role);
+    db.query(
+        'UPDATE employees SET ? WHERE ?',
+        [
+            {role_id: newRole},
+            {id: employeeId}
+        ],
+        function (err, res) {
+            if (err) throw err;
+            console.log(res.affectedRows + " employee's role updated!\n");
+        }
+    )
+};
+
+quit = () => {
+    inquirer.prompt([
+        {
+            type: 'confirm',
+            name: 'return',
+            message: 'Go Back To Main Menu?',
+            default: false
+        }
+    ])
+        .then(ans => {
+            if (ans.return) {
+                promptUser();
+            } else {
+                console.log("Goodbye");
+                db.end();
+            }
+        });
+};
+
+module.exports = {
+    viewAllDepartments,
+    viewAllRoles,
+    viewAllEmployees,
+    addADepartment,
+    addARole,
+    addAnEmployee,
+    updateAnEmployeeRole,
+    quit
+}
